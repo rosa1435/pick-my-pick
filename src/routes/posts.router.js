0107283e04286538ab06bbc8226bcce3e8115bb1
friend-0228 <kwj2712@gamil.com>
2024-03-12 22:09:28 +0900

    FEAT : 게시글 수정, 삭제

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,5 +1,5 @@
-import express from "express";
-// import { prisma } from '../utils/prisma/index.js';
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router();
 
@@ -10,7 +10,53 @@ const router = express.Router();
 // 게시글 상세 조회 API
 
 // 게시글 수정 API
+router.put('/posts/:postId', async (req, res, next) => {
+    try {
+        const { postId } = req.params;
+        const { title, content, startDate, endDate, multiVote, updatedAt, userId } = req.body;
+        if (!postId || !title || !content) return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+
+        const post = await prisma.posts.findFirst({ where: { id: +postId } });
+        if (!post) return res.status(404).json({ message: '존재하지 않는 게시글입니다.' });
+
+        const user = await prisma.users.findFirst({ where: { id: +userId } });
+
+        const updatedPost = await prisma.posts.update({
+            date: {
+                nickname: user.nickname,
+                title: title,
+                content: content,
+                startDate: startDate,
+                endDate: endDate,
+                multiVote: multiVote,
+                updatedAt: updatedAt,
+            },
+            where: { id: +postId },
+        });
+
+        return res.status(200).json({ data: updatedPost, message: '게시글을 수정하였습니다.' });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 // 게시글 삭제 API
+router.delete('/posts/:postId', async (req, res, next) => {
+    try {
+        const { postId } = req.params;
+        if (!postId) return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+
+        const post = await prisma.posts.findFirst({ where: { id: +postId } });
+        if (!post) return res.status(404).json({ message: '존재하지 않는 게시글입니다.' });
+
+        await prisma.posts.delete({ where: { id: +postId } });
+
+        return res.status(200).json({ message: '게시글을 삭제하였습니다.' });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 export default router;
