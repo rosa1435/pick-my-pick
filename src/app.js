3a22c8be97de0bb85fdd728d30ebf86011d38e8c
LetyPark <sunghee.leticia@gmail.com>
2024-03-12 22:09:26 +0900

    feat : 게시글 조회

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", [postsRouter]);
+app.use([postsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import { prisma } from '../utils/prisma/index.js';
+import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router();
 
@@ -8,7 +8,28 @@ const router = express.Router();
 // 게시글 전체 조회 API
 
 // 게시글 상세 조회 API
+router.get('/posts/:postId', async(req, res, next) =>{
 
+        const{postId} =req.params;
+
+        const post = await  prisma.posts.findFirst({
+            where: { id: parseInt(postId) },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                startDate: true,
+                endDate: true,
+                multiVote: true,
+                user: {
+                    select: {
+                        userName: true,
+                        nickname: true
+                    }
+                }
+            }
+        });
+        return res.status(200).json({post})});
 // 게시글 수정 API
 
 // 게시글 삭제 API
