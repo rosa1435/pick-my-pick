cd808da4f3c949c131c22b27557c704ed16c04c0
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-12 22:02:43 +0900

    feat : start

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,11 +1,44 @@
 import express from "express";
-// import { prisma } from '../utils/prisma/index.js';
+import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router();
 
 // 게시글 등록 API
+router.post('/posts', async (req, res, next) => {
+    const {title,content,startDate,endDate,multiVote,userId} = req.body;
 
+    const newPost = await prisma.posts.create({
+        data: {
+            title,
+            content,
+            startDate: new Date(startDate),
+            endDate: new Date(endDate), 
+            multiVote,
+            userId,
+            },
+        })
+        return res.status(201).json({newPost, message: "카테고리를 등록하였습니다." });
+});
+    
 // 게시글 전체 조회 API
+router.get('/posts', async (req, res,next) => {
+    const postList = await prisma.posts.findMany({
+        select: {
+            title: true,
+            content: true,
+            createdAt: true,
+            updatedAt: true,
+            startDate: true,
+            endDate: true,
+            multiVote: true,
+            likeCount: true,
+            commentsCount: true
+        }
+    });
+
+    return res.status(200).json({data: postList});
+
+});
 
 // 게시글 상세 조회 API
 
