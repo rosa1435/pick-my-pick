50a729a252d4a37190672b5c037fe4e825d906d5
LetyPark <sunghee.leticia@gmail.com>
2024-03-12 21:07:57 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import postsRouter from "./routes/posts.router.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/api", [postsRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
---
+++ src/routes/posts.router.js
@@ -0,0 +1,16 @@
+import express from "express";
+// import { prisma } from '../utils/prisma/index.js';
+
+const router = express.Router();
+
+// 게시글 등록 API
+
+// 게시글 전체 조회 API
+
+// 게시글 상세 조회 API
+
+// 게시글 수정 API
+
+// 게시글 삭제 API
+
+export default router;
