5b5b68046397f36a834b3bcd84437e2976658ac4
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-13 11:12:15 +0900

    feat : star2z1t

--- src/app.js
+++ src/app.js
@@ -6,12 +6,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // "*"는 모든 도메인에서의 요청을 허용함을 의미합니다. 보안상의 이유로, 실제 배포 환경에서는 특정 도메인을 지정하는 것이 좋습니다.
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //next();
-  });
-  
+
 
 app.use('/', [postsRouter]);
 
